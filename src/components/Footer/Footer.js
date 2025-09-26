ed2a7abbbe8af30ed2e21909e0f2fd79191152be
Kapra-buyerzkart <buyerzkart.technical@gmail.com>
2025-09-26 11:40:43 +0530

    Year in copyright updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <p>© 2023 KAPRA HIGHNESS VENTURES LTD. All rights reserved.</p>
+            <p>© 2025 KAPRA HIGHNESS VENTURES LTD. All rights reserved.</p>
         </footer>
     );
 };
