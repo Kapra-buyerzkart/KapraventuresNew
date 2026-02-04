939903efc1754da07149d8f486f7b4387e346444
jithin <jithinkm@buyerzkart.com>
2026-02-04 15:34:11 +0530

    new changes

--- src/constants/Employees.js
+++ src/constants/Employees.js
@@ -1,50 +1,46 @@
 import Rahul from "../../src/assets/images/Employees/rahul.webp"
 import Saru from "../../src/assets/images/Employees/saru.webp"
-import Suneer from "../../src/assets/images/Employees/suneer.webp"
-import Vijin from "../../src/assets/images/Employees/vijin.webp"
 import Jithin from "../../src/assets/images/Employees/jithin.webp"
-import Kiran from "../../src/assets/images/Employees/kiran.webp"
-import Anila from "../../src/assets/images/Employees/anila.webp"
 import Aleena from "../../src/assets/images/Employees/aleena.webp"
-import Dhanyan from "../../src/assets/images/Employees/dhanyan.webp"
-import Moorthy from "../../src/assets/images/Employees/moorthy.webp"
 import Person from "../../src/assets/images/Employees/person.png"
-import Nandhu from "../../src/assets/images/Employees/nandhu.webp"
-import Gouthamy from "../../src/assets/images/Employees/gouthamy.webp"
-import Aslam from "../../src/assets/images/Employees/aslam.webp"
 import Saneesh from "../../src/assets/images/Employees/saneesh.webp"
-import Rohit from "../../src/assets/images/Employees/rohit.webp"
 import Sunil from "../../src/assets/images/Employees/sunil.webp"
-import Nimmy from "../../src/assets/images/Employees/nimmy.webp"
-import Aadhil from "../../src/assets/images/Employees/aadhil.webp"
-import Remya from "../../src/assets/images/Employees/remya.webp"
-import Alex from "../../src/assets/images/Employees/alex.webp"
-import Anjali from "../../src/assets/images/Employees/anjali.webp"
+import Anu from "../../src/assets/images/Employees/anu.webp"
+import Navya from "../../src/assets/images/Employees/navya.webp"
+import Ritta from "../../src/assets/images/Employees/ritta.webp"
+import Fransa from "../../src/assets/images/Employees/fransa.webp"
+import Hr from "../../src/assets/images/Employees/hr.webp"
+import Rahil from "../../src/assets/images/Employees/rahil.webp"
+import Mariya from "../../src/assets/images/Employees/mariya.webp"
+import Niya from "../../src/assets/images/Employees/niya.webp"
+import Miju from "../../src/assets/images/Employees/miju.webp"
+import Sajil from "../../src/assets/images/Employees/sajil.webp"
+import Manju from "../../src/assets/images/Employees/manju.webp"
+import Sumith from "../../src/assets/images/Employees/sumith.webp"
+import Mohandas from "../../src/assets/images/Employees/mohandas.webp"
 
 const Employees = {
     "Person": Person,
     "Rahul": Rahul,
     "Saru": Saru,
-    "Suneer": Suneer,
-    "Vijin": Vijin,
     "Jithin": Jithin,
-    "Kiran": Kiran,
-    "Anila": Anila,
     "Aleena": Aleena,
-    "Dhanyan": Dhanyan,
-    "Moorthy": Moorthy,
-    "Nimmy": Nimmy,
-    "Nandhu": Nandhu,
     "Nisha": Person,
-    "Aslam": Aslam,
     "Saneesh": Saneesh,
-    "Rohit": Rohit,
     "Sunil": Sunil,
-    "Aadhil": Aadhil,
-    "Remya": Remya,
-    "Alex": Alex,
-    "Gouthamy": Gouthamy,
-    "Anjali": Anjali
+    "Anu": Anu,
+    "Navya": Navya,
+    "Ritta": Ritta,
+    "Fransa": Fransa,
+    "Hr": Hr,
+    "Rahil": Rahil,
+    "Mariya": Mariya,
+    "Niya": Niya,
+    "Miju": Miju,
+    "Sajil": Sajil,
+    "Manju": Manju,
+    "Sumith": Sumith,
+    "Mohandas": Mohandas
 }
 
 export default Employees;
--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -9,7 +9,7 @@ const teamMembers = [
   {
     name: "Peter S",
     designation: "Human Resources Manager",
-    photo: Employees.Person,
+    photo: Employees.Hr,
     company: "Kapra Group"
   },
   {
@@ -19,35 +19,119 @@ const teamMembers = [
     company: "Sellerzkart"
   },
   {
-    name: "Binu Chandran",
-    designation: "Jewellery Manager",
-    photo: Employees.Person,
-    company: "Kapra Gold and Diamonds"
+    name: "Miju KV",
+    designation: "Senior Operations Manager",
+    photo: Employees.Miju,
+    company: "Kapra Daily - HO"
   },
   {
-    name: "Krishna Moorthy PS",
-    designation: "Accounts Manager",
-    photo: Employees.Moorthy,
-    company: "Kapra Daily"
+    name: "Sajil Prasad",
+    designation: "CTO",
+    photo: Employees.Sajil,
+    company: "Kapra Web AI Makers"
   },
-  // {
-  //   name: "Remy MS",
-  //   designation: "Operation Manager-Hub",
-  //   photo: Employees.Remya,
-  //   company: "Sellerzkart"
-  // },
+  {
+    name: "Unni Thilakan",
+    designation: "Operations Head",
+    photo: Employees.Person,
+    company: "Kapra Daily - HO"
+  },
   {
     name: "Sunil PC",
     designation: "Store Manager",
     photo: Employees.Sunil,
-    company: "Kapra Daily"
+    company: "Kapra Daily - Chalakudy"
   },
-  // {
-  //   name: "Dhanyan PV",
-  //   designation: "Vendor Manager",
-  //   photo: Employees.Dhanyan,
-  //   company: "Kapra Daily"
-  // },
+  {
+    name: "Aju M",
+    designation: "Store Manager",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Manoj Kumar",
+    designation: "Store Manager",
+    photo: Employees.Person,
+    company: "Kapra Daily - Thripunithura"
+  },
+  {
+    name: "Muhammad Siraj",
+    designation: "Store Manager",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Amal Babu",
+    designation: "Assistant Store Manager",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Amal Aji",
+    designation: "Assistant Store Manager",
+    photo: Employees.Person,
+    company: "Kapra Daily - Thripunithura"
+  },
+  {
+    name: "Vinayan",
+    designation: "Assistant Showroom Manager",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Tirur"
+  },
+  {
+    name: "Aleena KS",
+    designation: "Accountant",
+    photo: Employees.Aleena,
+    company: "Sellerzkart"
+  },
+  {
+    name: "Anu John",
+    designation: "Accountant",
+    photo: Employees.Anu,
+    company: "Kapra Daily - HO"
+  },
+  {
+    name: "Kiran KU",
+    designation: "Store Supervisor",
+    photo: Employees.Person,
+    company: "Kapra Daily - Chalakudy"
+  },
+  {
+    name: "Rijin PJ",
+    designation: "Store Supervisor",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Muhammed Riswan MJ",
+    designation: "Store Supervisor",
+    photo: Employees.Person,
+    company: "Kapra Daily - Chalakudy"
+  },
+  {
+    name: "Arjun Manoj",
+    designation: "Store Supervisor",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Sumith Subramaniyan",
+    designation: "Construction Supervisor",
+    photo: Employees.Sumith,
+    company: "Kapra Daily - HO"
+  },
+  {
+    name: "Amjith",
+    designation: "Warehouse Incharge",
+    photo: Employees.Person,
+    company: "Sellerzkart"
+  },
+  {
+    name: "Atul",
+    designation: "Business Admin Manager",
+    photo: Employees.Person,
+    company: "Kapra Daily - HO"
+  },
   {
     name: "Jithin KM",
     designation: "Software Engineer",
@@ -58,121 +142,361 @@ const teamMembers = [
     name: "Saru Darai",
     designation: "Assistant Vendor Manager",
     photo: Employees.Saru,
-    company: "Kapra Daily"
+    company: "Kapra Daily - HO"
   },
   {
-    name: "Saneesh P",
-    designation: "Data Operation Assistant Manager",
-    photo: Employees.Saneesh,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Nisha Santhosh",
-    designation: "Assistant Store Manager",
+    name: "Aswathi Rajappan",
+    designation: "Accountant Executive",
     photo: Employees.Person,
-    company: "Kapra Daily"
+    company: "Kapra Gold and Diamonds - Thripunithura"
   },
   {
-    name: "Saurav Rajendran",
-    designation: "Assistant Store Manager",
+    name: "Shuhaib Aslam",
+    designation: "Accountant Executive",
     photo: Employees.Person,
-    company: "Kapra Daily"
-  },
-  // {
-  //   name: "Suneer M",
-  //   designation: "Marketing Officer",
-  //   photo: Employees.Suneer,
-  //   company: "Kapra Daily"
-  // },
-  {
-    name: "Aleena KS",
-    designation: "Accountant",
-    photo: Employees.Aleena,
-    company: "Sellerzkart"
+    company: "Kapra Gold and Diamonds - Tirur"
   },
   {
-    name: "Anu John",
-    designation: "Accountant",
-    photo: Employees.Person,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Unni Thilakan",
-    designation: "Store Supervisor",
-    photo: Employees.Person,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Muhammed Riswan MJ",
-    designation: "Store Supervisor",
-    photo: Employees.Person,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Rittamol Joseph",
-    designation: "Digital Marketing Specialist",
-    photo: Employees.Person,
+    name: "Muhammad Rahil",
+    designation: "UI/UX Designer",
+    photo: Employees.Rahil,
     company: "Kapra Web AI Makers"
   },
   {
     name: "Navya Das MK",
     designation: "Graphics Designer",
-    photo: Employees.Person,
+    photo: Employees.Navya,
     company: "Kapra Web AI Makers"
   },
-  // {
-  //   name: "Anila KS",
-  //   designation: "Data Entry Executive",
-  //   photo: Employees.Anila,
-  //   company: "Kapra Daily"
-  // },
   {
-    name: "Mariya Tomy",
-    designation: "Data Entry Executive",
+    name: "Rittamol Joseph",
+    designation: "Digital Marketing Specialist",
+    photo: Employees.Ritta,
+    company: "Kapra Web AI Makers"
+  },
+  {
+    name: "Vipin Kumar",
+    designation: "Sales",
     photo: Employees.Person,
-    company: "Kapra Daily"
+    company: "Kapra Gold and Diamonds - Thripunithura"
+  },
+  {
+    name: "Shybi",
+    designation: "Sales",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Thripunithura"
+  },
+  {
+    name: "Shari",
+    designation: "Reception",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Thripunithura"
+  },
+  {
+    name: "Suresh Babu",
+    designation: "Smith",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Thripunithura"
+  },
+  {
+    name: "Saneesh P",
+    designation: "Data Entry Operator Head",
+    photo: Employees.Saneesh,
+    company: "Kapra Daily - HO"
   },
   {
     name: "Fransa Mol TF",
-    designation: "Data Entry Executive",
+    designation: "Data Entry Operator",
+    photo: Employees.Fransa,
+    company: "Kapra Daily - HO"
+  },
+  {
+    name: "Manju MM",
+    designation: "Data Entry Operator",
+    photo: Employees.Manju,
+    company: "Kapra Daily - HO"
+  },
+  {
+    name: "Mariya Tomy",
+    designation: "Data Entry Operator",
+    photo: Employees.Mariya,
+    company: "Kapra Daily - HO"
+  },
+  {
+    name: "Niya Paul",
+    designation: "Warehouse Executive",
+    photo: Employees.Niya,
+    company: "Kapra Daily - HO"
+  },
+  {
+    name: "Vipin M",
+    designation: "Data Entry Operator",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Musammil M",
+    designation: "Data Entry Operator",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Ajitha N",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Ajmal",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Aleena Johnson",
+    designation: "Store Executive - Chalakudy",
     photo: Employees.Person,
     company: "Kapra Daily"
   },
   {
-    name: "Indu C",
+    name: "Anandhu G",
     designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Ashik Baiju",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Chalakudy"
+  },
+  {
+    name: "Abhinand Suresh",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Thripunithura"
+  },
+  {
+    name: "Bincy NS",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Chinchu P",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Haleemath Sahadiya KP",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Jishnu",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Jose PT",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Chalakudy"
+  },
+  {
+    name: "Lekha N",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Malini Anil",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Chalakudy"
+  },
+  {
+    name: "Maya M",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Muhammed Farhan",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Malavika",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Thripunithura"
+  },
+  {
+    name: "Neeraj Babu",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Thripunithura"
+  },
+  {
+    name: "Rahul VR",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Thripunithura"
+  },
+  {
+    name: "Rasheeda VP",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Saranya Babu",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Shyma Mathiew",
+    designation: "Store Executive - Chalakudy",
     photo: Employees.Person,
     company: "Kapra Daily"
   },
   {
-    name: "Gouthamy Gopakumar",
-    designation: "Store Executive",
-    photo: Employees.Gouthamy,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Honeymol PK",
+    name: "Sreeja PP",
     designation: "Store Executive",
     photo: Employees.Person,
-    company: "Kapra Daily"
+    company: "Kapra Daily - Tirur"
   },
   {
-    name: "Abinanth Suresh",
+    name: "Sreenandan L",
     designation: "Store Executive",
     photo: Employees.Person,
-    company: "Kapra Daily"
+    company: "Kapra Daily - Kanichukulangara"
   },
   {
-    name: "Maria Sandra",
+    name: "Subair",
     designation: "Store Executive",
     photo: Employees.Person,
-    company: "Kapra Daily"
+    company: "Kapra Daily - Tirur"
   },
   {
-    name: "Akshay PM",
+    name: "Sumitha P",
     designation: "Store Executive",
     photo: Employees.Person,
-    company: "Kapra Daily"
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Suraj Suresh",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Usha M",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Vijina CV",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Raju ER",
+    designation: "Delivery & Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Chalakudy"
+  },
+  // {
+  //   name: "Mohandas",
+  //   designation: "Product Advisor",
+  //   photo: Employees.Mohandas,
+  //   company: "Sellerzkart"
+  // },
+  {
+    name: "Mubeena Sherin CK",
+    designation: "Data Entry Operator",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Indu C Nair",
+    designation: "Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Vennala"
+  },
+  {
+    name: "Safna",
+    designation: "Office Assistant",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Tirur"
+  },
+  {
+    name: "Surya",
+    designation: "Sales Executive",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Tirur"
+  },
+  {
+    name: "Mini Selvan",
+    designation: "House Keeping & Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Tirur"
+  },
+  {
+    name: "Ajay Pradeesh",
+    designation: "House Keeping",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Ambily VA",
+    designation: "House Keeping",
+    photo: Employees.Person,
+    company: "Kapra Daily - Kanichukulangara"
+  },
+  {
+    name: "Smitha K",
+    designation: "House Keeping",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Sudha Kumari KV",
+    designation: "House Keeping",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Saritha Prasad",
+    designation: "Cleaning Staff & Store Executive",
+    photo: Employees.Person,
+    company: "Kapra Daily - Tirur"
+  },
+  {
+    name: "Sheeba Paul",
+    designation: "House Keeping",
+    photo: Employees.Person,
+    company: "Kapra Daily - HO"
+  },
+  {
+    name: "Sudha",
+    designation: "House Keeping",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Tirur"
+  },
+  {
+    name: "Manju",
+    designation: "Cleaning",
+    photo: Employees.Person,
+    company: "Kapra Gold and Diamonds - Thripunithura"
   },
   // {
   //   name: "Akshay PM",
@@ -180,61 +504,12 @@ const teamMembers = [
   //   photo: Employees.Person,
   //   company: "Kapra Daily"
   // },
-  {
-    name: "Abhil EM",
-    designation: "Delivery Executive",
-    photo: Employees.Person,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Michael KJ",
-    designation: "Delivery Executive",
-    photo: Employees.Person,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Sheeba Paul",
-    designation: "House Keeping",
-    photo: Employees.Person,
-    company: "Kapra Daily"
-  },
-  {
-    name: "Sindhu KS",
-    designation: "House Keeping",
-    photo: Employees.Person,
-    company: "Kapra Daily"
-  },
   // {
-  //   name: "Aadhil Sameer",
-  //   designation: "Store Executive",
-  //   photo: Employees.Aadhil,
+  //   name: "Abhil EM",
+  //   designation: "Delivery Executive",
+  //   photo: Employees.Person,
   //   company: "Kapra Daily"
   // },
-  // {
-  //   name: "Nimmy John",
-  //   designation: "Front Desk Executive",
-  //   photo: Employees.Nimmy,
-  //   company: "Kapra Daily"
-  // },
-  // {
-  //   name: "Muhammad Aslam PA",
-  //   designation: "Junior Data Entry Operator",
-  //   photo: Employees.Aslam,
-  //   company: "Kapra Daily"
-  // },
-  // {
-  //   name: "Rahul VR",
-  //   designation: "Software Engineer",
-  //   photo: Employees.Rahul,
-  //   company: "Kapra Daily"
-  // },
-  // {
-  //   name: "Saru Darai",
-  //   designation: "Assistant Vendor Manager",
-  //   photo: Employees.Saru,
-  //   company: "Kapra Daily"
-  // }
-
 ];
 
 const Team = () => {
@@ -250,7 +525,9 @@ const Team = () => {
       <div className="team-row">
         {teamMembers.map((member, index) => (
           <div key={index} className="team-card">
-            <img src={member.photo} alt={PersonImage} className="team-image" />
+            <div className="team-image-wrapper">
+              <img src={member.photo} alt={member.name} className="team-image" />
+            </div>
             <h3 className="team-name">{member.name}</h3>
             <p className="team-designation">{member.designation}</p>
             <p className="team-designation">{member.company}</p>
